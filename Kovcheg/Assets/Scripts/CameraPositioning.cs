61c308fdc2f28a9cdd475c0d98096e462217959a
Fel6h <156591319+Fel6h@users.noreply.github.com>
2024-01-23 20:48:12 +0300

    Update CameraPositioning.cs

--- Kovcheg/Assets/Scripts/CameraPositioning.cs
+++ Kovcheg/Assets/Scripts/CameraPositioning.cs
@@ -1,17 +1,16 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class CameraPositioning : MonoBehaviour
 {
     [SerializeField] private Transform _camera;
-    [SerializeField] private Transform startCameraPosition;
+    [SerializeField] private Transform cameraParent;
+    [SerializeField] private Transform player;
     [SerializeField] private float scrollDepth = 3;
     [SerializeField] private float smoothScrolling = 10;
-    private List<Vector3> positions = new List<Vector3>();
-    private float scrollStep;
+    private List<Vector3> cameraPositions = new List<Vector3>();
     private int indexCurrentPostion;
+    private Vector3 playerPositionLastFrame;
 
     private int IndexCurrentPostion
     {
@@ -19,8 +18,8 @@ public class CameraPositioning : MonoBehaviour
         {
             if (indexCurrentPostion < 0)
                 indexCurrentPostion = 0;
-            if (indexCurrentPostion == positions.Count)
-                indexCurrentPostion = positions.Count - 1;
+            if (indexCurrentPostion == cameraPositions.Count)
+                indexCurrentPostion = cameraPositions.Count - 1;
 
             return indexCurrentPostion;
         }
@@ -28,30 +27,30 @@ public class CameraPositioning : MonoBehaviour
 
     private void Start()
     {
-        scrollStep = scrollDepth / smoothScrolling;
         indexCurrentPostion = ((int)smoothScrolling);
+        playerPositionLastFrame = player.position;
+        FillListPositions();
     }
 
     private void FixedUpdate()
     {
-        FillListPositions();
         SetPosition();
     }
 
     private void FillListPositions()
     {
-        Vector3 target = startCameraPosition.localPosition;
-        target.y -= scrollDepth;
-        target.z += scrollDepth;
+        Vector3 endCameraPosition = _camera.localPosition;
+        endCameraPosition.y -= scrollDepth;
+        endCameraPosition.z += scrollDepth;
 
-        positions.Clear();
+        float scrollStep = scrollDepth / smoothScrolling;
 
         for (int i = 0; i < smoothScrolling + 1; i++)
         {
-            Vector3 newPosition = target;
+            Vector3 newPosition = endCameraPosition;
             newPosition.y += scrollStep * i;
             newPosition.z -= scrollStep * i;
-            positions.Add(newPosition);
+            cameraPositions.Add(newPosition);
         }
     }
 
@@ -68,6 +67,19 @@ public class CameraPositioning : MonoBehaviour
             indexCurrentPostion--;
         }
 
-        _camera.localPosition = Vector3.MoveTowards(_camera.localPosition, positions[IndexCurrentPostion], 0.1f);
+        if (_camera.localPosition != cameraPositions[IndexCurrentPostion])
+        {
+            _camera.localPosition = Vector3.MoveTowards(_camera.localPosition, cameraPositions[IndexCurrentPostion], 0.1f);
+        }
+
+        if (playerPositionLastFrame != player.position)
+        {
+            Vector3 direction = player.position - playerPositionLastFrame;
+            direction.y = 0;
+
+            cameraParent.position += direction;
+        }
+
+        playerPositionLastFrame = player.position;
     }
 }
